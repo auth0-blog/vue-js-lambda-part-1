080c62bf3cd567b65d88e91134671ef094a6d555
Bruno Krebs <bruno.krebs@auth0.com>
2018-10-16 09:58:41 -0300

    Scaffolding the Express web application.

---
+++ backend/src/index.js
@@ -0,0 +1,26 @@
+//import dependencies
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+// define the Express app
+const app = express();
+
+// enhance your app security with Helmet
+app.use(helmet());
+
+// use bodyParser to parse application/json content-type
+app.use(bodyParser.json());
+
+// enable all CORS requests
+app.use(cors());
+
+// log HTTP requests
+app.use(morgan('combined'));
+
+// start the server
+app.listen(8081, () => {
+  console.log('listening on port 8081');
+});
