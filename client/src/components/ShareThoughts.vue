dcf0e2bb1e0772bce6359e6b8732cd455711f4cc
Bruno Krebs <bruno.krebs@auth0.com>
2018-10-16 11:12:50 -0300

    adding a secured route to Vue.js

---
+++ client/src/components/ShareThoughts.vue
@@ -0,0 +1,26 @@
+<script>
+import MicroPostsService from '../MicroPostsService'
+
+export default {
+  name: 'ShareThoughts',
+  data () {
+    return {
+      text: ''
+    }
+  },
+  methods: {
+    async shareThoughts () {
+      await MicroPostsService.insertMicroPost(this.text)
+      this.$router.push('/')
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="share-thoughts">
+    <label for="share-thoughts">What do you want to share?</label>
+    <input id="share-thoughts" v-model="text" placeholder="Be nice!"/>
+    <button v-on:click="shareThoughts">Share!</button>
+  </div>
+</template>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Callback from '@/components/Callback'
+import ShareThoughts from '@/components/ShareThoughts'
+import auth0Client from '../AuthService'
 
 Vue.use(Router)
 
@@ -17,6 +19,17 @@ export default new Router({
       path: '/callback',
       name: 'Callback',
       component: Callback
+    },
+    {
+      path: '/share-your-thoughts',
+      name: 'ShareThoughts',
+      component: ShareThoughts,
+      beforeEnter: (to, from, next) => {
+        if (auth0Client.isAuthenticated()) {
+          return next()
+        }
+        next('/')
+      }
     }
   ]
 })
